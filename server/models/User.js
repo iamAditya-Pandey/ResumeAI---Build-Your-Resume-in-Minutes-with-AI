2c4d017897000daafe9be03ac576dd5cf81fdcf2
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-03-05 03:09:42 +0530

    fix(models): update User schema constraints

--- server/models/User.js
+++ server/models/User.js
@@ -19,6 +19,7 @@ UserSchema.methods.comparePassword = function (password){
     return bcrypt.compareSync(password, this.password)
 }
 
+
 const User = mongoose.model("User", UserSchema)
 
 export default User;
