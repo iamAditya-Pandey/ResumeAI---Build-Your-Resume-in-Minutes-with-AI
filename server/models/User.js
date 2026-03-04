054a908818db6b460fc334ab82f668304d1a8d0d
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-03-05 03:01:50 +0530

    feat(models): define User database schema

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+/*
+ * @project ResumeAI
+ * @file User.js
+ * @author Aditya Pandey
+ * @github https://github.com/iamAditya-Pandey
+ * * Copyright (c) 2026 Aditya Pandey. All rights reserved.
+ */
+
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt'
+
+const UserSchema = new mongoose.Schema({
+    name: {type: String, required: true },
+    email: {type: String, required: true, unique: true },
+    password: {type: String, required: true },
+}, {timestamps: true })
+
+UserSchema.methods.comparePassword = function (password){
+    return bcrypt.compareSync(password, this.password)
+}
+
+const User = mongoose.model("User", UserSchema)
+
+export default User;
