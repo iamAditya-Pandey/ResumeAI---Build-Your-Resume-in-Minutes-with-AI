da7b313452e1c980864072bed49919f64d91c14c
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-03-05 03:09:43 +0530

    fix(routes): correct resume routing logic

--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -7,6 +7,7 @@
  */
 
 import express from "express";
+
 import protect from "../middlewares/authMiddleware.js";
 import { createResume, deleteResume, getPublicResumeById, getResumeById, updateResume } from "../controllers/resumeController.js";
 import upload from "../configs/multer.js";
@@ -18,5 +19,6 @@ resumeRouter.put('/update', upload.single('image'), protect, updateResume);
 resumeRouter.delete('/delete/:resumeId', protect, deleteResume);
 resumeRouter.get('/get/:resumeId', protect, getResumeById);
 resumeRouter.get('/public/:resumeId', getPublicResumeById);
+
 
 export default resumeRouter
