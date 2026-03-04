9e76dfda7ee6a434ff4e411578d7c07b33bc14a0
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-03-05 03:01:47 +0530

    feat(config): setup MongoDB connection

---
+++ server/configs/db.js
@@ -0,0 +1,32 @@
+/*
+ * @project ResumeAI
+ * @file db.js
+ * @author Aditya Pandey
+ * @github https://github.com/iamAditya-Pandey
+ * * Copyright (c) 2026 Aditya Pandey. All rights reserved.
+ */
+
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on("connected", ()=>{console.log("Database connected successfully")})
+
+        let mongodbURI = process.env.MONGODB_URI;
+        const projectName = 'resume-builder';
+
+        if(!mongodbURI){
+            throw new Error("MONGODB_URI environment variable not set")
+        }
+
+        if(mongodbURI.endsWith('/')){
+            mongodbURI = mongodbURI.slice(0, -1)
+        }
+
+        await mongoose.connect(`${mongodbURI}/${projectName}`)
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error)
+    }
+}
+
+export default connectDB;
