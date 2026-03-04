fc1019c2551abd8563afd875b4b301c9e3583078
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-03-05 03:09:43 +0530

    refactor(routes): clean up AI route middleware chains

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -7,6 +7,7 @@
  */
 
 import express from "express";
+
 import protect from "../middlewares/authMiddleware.js";
 import { enhanceJobDescription, enhanceProfessionalSummary, uploadResume} from "../controllers/aiController.js";
 
@@ -18,5 +19,6 @@ const aiRouter = express.Router();
 aiRouter.post('/enhance-pro-sum', protect, enhanceProfessionalSummary)
 aiRouter.post('/enhance-job-desc', protect, enhanceJobDescription)
 aiRouter.post('/upload-resume', protect, uploadResume)
+
 
 export default aiRouter
