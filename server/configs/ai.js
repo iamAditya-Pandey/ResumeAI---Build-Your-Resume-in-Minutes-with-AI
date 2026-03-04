7b9f67419acdd8a8d630e1d6523a967a2814352b
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-03-05 03:01:47 +0530

    feat(config): integrate AI service

---
+++ server/configs/ai.js
@@ -0,0 +1,16 @@
+/*
+ * @project ResumeAI
+ * @file ai.js
+ * @author Aditya Pandey
+ * @github https://github.com/iamAditya-Pandey
+ * * Copyright (c) 2026 Aditya Pandey. All rights reserved.
+ */
+
+import OpenAI from "openai";
+
+const ai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    baseURL: process.env.OPENAI_BASE_URL,
+});
+
+export default ai
