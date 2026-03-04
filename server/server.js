847d5fa2c64bd34c799856e0a84e283b6bb49b21
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-03-05 03:01:52 +0530

    feat: initialize main Express server and middleware

---
+++ server/server.js
@@ -0,0 +1,34 @@
+/*
+ * @project ResumeAI
+ * @file server.js
+ * @author Aditya Pandey
+ * @github https://github.com/iamAditya-Pandey
+ * * Copyright (c) 2026 Aditya Pandey. All rights reserved.
+ */
+
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+import resumeRouter from "./routes/resumeRoutes.js";
+import aiRouter from "./routes/aiRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Database connection
+await connectDB()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res)=> res.send("Server is live..."))
+app.use('/api/users', userRouter)
+app.use('/api/resumes', resumeRouter)
+app.use('/api/ai', aiRouter)
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+    
+});
