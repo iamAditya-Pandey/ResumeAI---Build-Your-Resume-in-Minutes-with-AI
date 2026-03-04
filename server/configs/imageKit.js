b9863e7fb8a99762a832cc4be71789fdeb8a919c
Aditya Pandey <pandeyaditya19012006@gmail.com>
2026-03-05 03:01:48 +0530

    feat(config): configure ImageKit for media handling

---
+++ server/configs/imageKit.js
@@ -0,0 +1,16 @@
+/*
+ * @project ResumeAI
+ * @file imageKit.js
+ * @author Aditya Pandey
+ * @github https://github.com/iamAditya-Pandey
+ * * Copyright (c) 2026 Aditya Pandey. All rights reserved.
+ */
+
+import ImageKit from '@imagekit/nodejs';
+
+const imagekit = new ImageKit({
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
+
+
+export default imagekit
